fc6a4e6bedb3cd06cf2bdc8ca6e56f3fc129bb53
prajin <prajinpatil42@gmail.com>
2023-08-22 13:19:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// const btnon = document.getElementById('on');
+
+// const btnoff = document.getElementById('off');
+
+// btnon.addEventListener('click', function onClick() {
+  
+//   document.body.style.backgroundColor = 'black';
+//   document.body.style.color = 'green';
+
+// });
+
+
+
+// btnoff.addEventListener('click', function onClick() {
+
+//     document.body.style.backgroundColor = 'purple';
+//     document.body.style.color = 'yellow';
+  
+  
+//   });
